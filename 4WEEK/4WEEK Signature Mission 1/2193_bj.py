29ef3c99e3a293aa4cda440b20379ce86312d621
gypig <gypig12@gachon.ac.kr>
2022-07-10 20:54:02 +0900

    4WEEK-1 PREV 미션 풀이

--- 4WEEK/4WEEK Signature Mission 1/2193_bj.py
+++ 4WEEK/4WEEK Signature Mission 1/2193_bj.py
@@ -1,12 +1,11 @@
-import sys
-input = sys.stdin.readline
+N = int(input())
 
-n = int(input())
+dp = [0] * (101)
+dp[1],dp[2],dp[3],dp[4],dp[5], = 1,1,1,2,2
 
-dp = [0] * (n + 1)
-dp[1], dp[2] = 1, 1
+for i in range(6, 101):
+    dp[i] = dp[i-2] + dp[i-3]
 
-for i in range(3, n+1):
-    dp[i] = dp[i-2] + dp[i-1]
-
-print(dp[n])
+for _ in range(N):
+    T = int(input())
+    print(dp[T])
