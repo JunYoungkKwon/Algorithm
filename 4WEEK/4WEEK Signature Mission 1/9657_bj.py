bdcf4a02ef2a7ade050db194758f5290a3a268b4
gypig <gypig12@gachon.ac.kr>
2022-07-11 13:36:09 +0900

    4WEEK-1 PREV 미션 풀이

---
+++ 4WEEK/4WEEK Signature Mission 1/9657_bj.py
@@ -0,0 +1,13 @@
+N = int(input())
+dp = [0] * 1001
+dp[1], dp[2], dp[3], dp[4] = 1, 0, 1, 1
+
+for i in range(5, N+1):
+    if dp[i-1] and dp[i-3] and dp[i-4]:
+        dp[i] = 0
+    else:
+        dp[i] = 1
+if dp[N]:
+    print("SK")
+else:
+    print("CY")
