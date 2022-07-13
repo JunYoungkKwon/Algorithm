b276d31f05c7c21d559f0cda1e1cca62c99a9cea
gypig <gypig12@gachon.ac.kr>
2022-07-13 09:18:40 +0900

    4WEEK-1 PREV 미션 풀이

---
+++ 4WEEK/4WEEK Signature Mission 1/11052_bj.py
@@ -0,0 +1,11 @@
+K = int(input())
+card = [0] + list(map(int, input().split()))
+dp = [0] * (K+1)
+
+for i in range(1, K+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], card[j]+ dp[i - j])
+print(dp[-1])
+
+
+
