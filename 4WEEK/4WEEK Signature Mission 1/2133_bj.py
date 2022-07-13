4df72aec7e67715189e0b8a642e5dfc470123018
gypig <gypig12@gachon.ac.kr>
2022-07-13 22:58:34 +0900

    4WEEK-1 PREV 미션 풀이

---
+++ 4WEEK/4WEEK Signature Mission 1/2133_bj.py
@@ -0,0 +1,10 @@
+#이전 타일은 점화식 찾기 쉬웠는데..
+#이번 건 점화식이 어려워서 참고했음
+N = int(input())
+cases = [0] * 31
+cases[2] = 3
+
+for i in range(4, N+1, 2):
+    cases[i] = 2 + cases[i-2] * 3 + sum(cases[:i-2]) * 2
+
+print(str(cases[N]))
