4bcc8b143e3d3923b64758eafb10d14e6baccc34
gypig <gypig12@gachon.ac.kr>
2022-07-18 14:43:16 +0900

    5WEEK-1 PREV 미션 풀이

--- 5WEEK/5WEEK Signature Mission 1/13458_bj.py
+++ 5WEEK/5WEEK Signature Mission 1/13458_bj.py
@@ -1,2 +1,16 @@
-N, M = map(int,input().split())
-print(N*M-1)
+# 문제 이해를 잘못했음 총감독이 무조건 있어야 함..
+N = int(input())
+li = list(map(int, input().split()))
+A, B = map(int, input().split())
+ans = 0
+for stu in li:
+        stu -= A
+        ans += 1
+        if stu > 0:
+            if stu % B:
+                ans += (stu // B) + 1
+            else:
+                ans += (stu // B) + 0
+
+print(ans)
+
