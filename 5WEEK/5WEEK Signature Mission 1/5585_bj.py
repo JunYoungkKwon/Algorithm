a26f89ad66162b84abce883aeafb57d37a6ca8f3
gypig <gypig12@gachon.ac.kr>
2022-07-18 14:53:24 +0900

    5WEEK-1 PREV 미션 풀이

--- 5WEEK/5WEEK Signature Mission 1/5585_bj.py
+++ 5WEEK/5WEEK Signature Mission 1/5585_bj.py
@@ -1,2 +1,13 @@
-N, M = map(int,input().split())
-print(N*M-1)
+money = int(input())
+money = 1000 - money
+chg = [500, 100, 50, 10, 5, 1]
+ans = 0
+if money:
+    for ch in chg:
+        ans += (money // ch)
+        money %= ch
+
+    print(ans)
+
+else:
+    print(ans)
