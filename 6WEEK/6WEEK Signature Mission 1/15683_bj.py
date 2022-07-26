26c2aa62d2eeeaa6840001a5ed492f3c76e7ffc4
gypig <gypig12@gachon.ac.kr>
2022-07-26 14:00:33 +0900

    6WEEK-1 PREV 미션 풀이

--- 6WEEK/6WEEK Signature Mission 1/15683_bj.py
+++ 6WEEK/6WEEK Signature Mission 1/15683_bj.py
@@ -1,68 +1,73 @@
-N = int(input())
-board = [list(input()) for _ in range(N)]
-check = [[0] * N for _ in range(N)]
-#red = C, blue = P, green = Z, yellow = Y
-color = [0] * 4
-dy = [1, 0, -1, 0]
-dx = [0, 1, 0, -1]
-x, y = 0, 0
+#혼자 하다가 헤메서 결국 참고함
+import copy
+
 def is_valid(y, x):
-    return 0 <= y < N and 0 <= x < N
-def color_chk(y, x, ny, nx):
-    if board[y][x] == board[ny][nx]:
-        return False
-    else:
-        return True
-def change_color(y, x, ny, nx):
-    a = board[y][x]
-    board[y][x] = board[ny][nx]
-    board[ny][nx] = a
+    return 0 <= y < N and 0 <= x < M
 
-def count_color():
-    ans = 1
+def watch(y, x, direction, board):
+    for d in direction:
+        ny, nx = y, x
+        # 특정 방향으로 벽을 만나거나 사무실을 벗어나기 전까지 탐색
+        while True:
+            ny += direction_list[d][0]
+            nx += direction_list[d][1]
+            # 맵 내 위치
+            if is_valid(ny, nx):
+                # 벽을 만난 경우
+                if board[ny][nx] == 6:
+                    break
+                # 새로운 감시가능 영역일 경우
+                elif board[ny][nx] == 0:
+                    board[ny][nx] = '#'
+            # 맵 외 위치
+            else:
+                break
+
+def cnt_zero(board):
+    cnt = 0
     for i in range(N):
-        cnt = 1
-        for j in range(1, N):
-            if board[i][j] == board[i][j - 1]:
+        for j in range(M):
+            if board[i][j] == 0:
                 cnt += 1
-            else:
-                cnt = 1
-            ans = max(ans, cnt)
+    return cnt
 
-        cnt = 1
-        for j in range(1, N):
-            if board[j][i] == board[j - 1][i]:
-                cnt += 1
-            else:
-                cnt = 1
-            ans = max(ans, cnt)
-    return ans
-
-
-
-result = 0
-while True:
-    #다 돌았다면 종료
-    if y == N:
-        break
-    for k in range(4):
-        ny = y + dy[k]
-        nx = x + dx[k]
-        if is_valid(ny, nx):
-            # 1.상근이는 사탕의 색이 다른 인접한 두 칸을 고른다.
-            if color_chk(y, x, ny, nx):
-                # 2.그 다음 고른 칸에 들어있는 사탕을 서로 교환한다.
-                change_color(y, x, ny, nx)
-                # 3.모두 같은 색으로 이루어져 있는 가장 긴 연속 부분(행 또는 열)을 고른 다음 그 사탕을 모두 먹는다.
-                # for i in range(N):
-                #     print(*board[i])
-                # print(f'result = {result}, x = {x},y = {y}, nx = {nx}, ny = {ny}')
-                result = max(result, count_color())
-                change_color(y, x, ny, nx)  # 원상복구
-
-    if x < N-1:
-        x = x + 1
+def dfs(board, depth):
+    global answer
+    # 종료 조건: 모든 CCTV 탐색
+    if depth == len(cctv):
+        # 사각지대 최솟값
+        answer = min(answer, cnt_zero(board))
+        return
     else:
-        y = y + 1
-        x = 0
-print(result)
+        # 사무실 정보 깊은 복사
+        graph_copy = copy.deepcopy(board)
+        y, x, cctv_type = cctv[depth]
+        for cctv_dir in cctv_direction[cctv_type]:
+            # CCTV 감시영역 구하는 함수 호출
+            watch(y, x, cctv_dir, graph_copy)
+            # 현재 Case에서 타 모든 CCTV 재귀적 탐색
+            dfs(graph_copy, depth + 1)
+            # CCTV를 다른 방향으로 회전시킨 후 재탐색하기 위함
+            graph_copy = copy.deepcopy(board)
+
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+cctv = []
+answer = int(1e9)
+for i in range(N):
+    for j in range(M):
+        if 1 <= board[i][j] <= 5:
+            cctv.append((i, j, board[i][j]))
+direction_list = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+# CCTV별 이동 가능한 방향
+cctv_direction = [
+    [],
+    [[0], [1], [2], [3]],  # 1번 CCTV
+    [[0, 1], [2, 3]],  # 2번 CCTV
+    [[0, 2], [0, 3], [1, 2], [1, 3]],  # 3번 CCTV
+    [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]],  # 4번 CCTV
+    [[0, 1, 2, 3]]  # 5번 CCTV
+]
+
+dfs(board, 0)
+print(answer)
