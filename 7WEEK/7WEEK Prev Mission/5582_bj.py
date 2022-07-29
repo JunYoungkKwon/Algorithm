9f8b19431dc7a88cfd16ff1954028092aea555d9
gypig <gypig12@gachon.ac.kr>
2022-07-29 21:43:07 +0900

    7WEEK-1 PREV 미션 풀이

--- 7WEEK/7WEEK Prev Mission/5582_bj.py
+++ 7WEEK/7WEEK Prev Mission/5582_bj.py
@@ -1,5 +1,11 @@
 str1 = input()
 str2 = input()
-le1 = len(str1)
-le2 = len(str2)
-board = [[0] * le2 for _ in range(le2)]
+dp = [[0] * (len(str2)+1) for _ in range(len(str1)+1)]
+ans = 0
+for i in range(1, len(str1)+1):
+    for j in range(1, len(str2)+1):
+        if (str1[i-1] == str2[j-1]):
+            dp[i][j] = dp[i-1][j-1] + 1
+            ans = max(dp[i][j], ans)
+
+print(ans)
