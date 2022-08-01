7f733928360827370112f5516956e1bd1c0393eb
gypig <gypig12@gachon.ac.kr>
2022-08-01 21:14:31 +0900

    7WEEK-1 PREV 미션 풀이

--- 7WEEK/7WEEK Signature Mission 1/14719_bj.py
+++ 7WEEK/7WEEK Signature Mission 1/14719_bj.py
@@ -1,35 +1,15 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-def bfs(i, j):
-    q = deque()
-    q.append([i, j])
-    max_n = 0
-    while q:
-        a, b = q.popleft()
-        for k in range(4):
-            x = a + dx[k]
-            y = b + dy[k]
-            if 0 <= x < n and 0 <= y < m and visit[x][y] == 0 and s[x][y] != "W":
-                visit[x][y] = 1
-                s[x][y] = s[a][b] + 1
-                print(f'ny ={y} nx = {x} nd = {s[x][y]}')
-                max_n = max(max_n, s[x][y])
-                q.append([x, y])
-    return max_n
-n, m = map(int, input().split())
-s = []
-result = 0
-for i in range(n):
-    s.append(list(input().strip()))
-for i in range(n):
-    for j in range(m):
-        if s[i][j] != "W":
-            print(f'i ={i} j = {j}')
-            visit = [[0] * m for _ in range(n)]
-            s[i][j] = 0
-            visit[i][j] = 1
-            result = max(result, bfs(i, j))
-print(result)
+N, M = map(int, input().split())
+rain = list(map(int, input().split()))
+
+ans = 0
+for i in range(1, M - 1):
+    l = max(rain[:i])
+    r = max(rain[i+1:])
+
+    compare = min(l, r)
+
+    if rain[i] < compare:
+        ans += compare - rain[i]
+
+print(ans)
+
