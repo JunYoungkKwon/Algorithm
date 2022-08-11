0de71df0042609c407657a87977179202efcd428
gypig <gypig12@gachon.ac.kr>
2022-08-11 14:00:40 +0900

    9WEEK- 문제 풀이

---
+++ 9WEEK/9WEEK Signature Mission 1/15596_bj.py
@@ -0,0 +1,3 @@
+def solve(a):
+    return sum(a)
+print(solve([1, 2, 3, 4, 5]))
--- 9WEEK/9WEEK Signature Mission 1/4344_bj.py
+++ 9WEEK/9WEEK Signature Mission 1/4344_bj.py
@@ -1,7 +1,10 @@
-N = int(input())
-score = list(map(int, input().split()))
-max_score = max(score)
-ans = 0
-for sco in score:
-    ans += (sco/max_score) * 100
-print(ans/N)
+for _ in range(int(input())):
+    N, *score = list(map(int, input().split()))
+    mid_score = sum(score) / N
+    cnt = 0
+    for k in score:
+        if k > mid_score:
+            cnt += 1
+    ans = (cnt / N) * 100
+    score = '{:.3f}'.format(round(ans, 3))
+    print(f"{score}%")
