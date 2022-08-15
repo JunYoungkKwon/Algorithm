ceb074e9cabdacafcfd00f9cba188a3102e0c95e
gypig <gypig12@gachon.ac.kr>
2022-08-15 21:50:02 +0900

    10WEEK- 문제 풀이

--- 10WEEK/10WEEK Signature Mission 1/1929_bj.py
+++ 10WEEK/10WEEK Signature Mission 1/1929_bj.py
@@ -4,7 +4,7 @@ sosu_li = []
 for i in range(M, N+1):
     sosu = True
     if i > 1:
-        for j in range(2, i):
+        for j in range(2, int(i** 0.5)+1):
             if i % j == 0:
                 sosu = False
                 break
