7d102b2ab95b6060f22264b9f23a8a5c02316b6f
gypig <gypig12@gachon.ac.kr>
2022-08-25 22:31:57 +0900

    10WEEK- 문제 풀이

--- 11WEEK/11WEEK Signature Mission 1/11729_bj.py
+++ 11WEEK/11WEEK Signature Mission 1/11729_bj.py
@@ -3,7 +3,6 @@ def hanoi_tower(n, start, end):
         print(start, end)
         return
 
-
     hanoi_tower(n - 1, start, 6 - start - end)  # 1단계
     print(start, end)  # 2단계
     hanoi_tower(n - 1, 6 - start - end, end)  # 3단계
