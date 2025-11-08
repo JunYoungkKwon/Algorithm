fb505d2e36560452d47f177dbac6716430403b18
JUNYOUNG <gypig12@gachon.ac.kr>
2025-11-08 21:04:21 +0900

    Me

---
+++ 6WEEK TEST/6WEEK Prev Mission/15686_bj.py
@@ -0,0 +1,8 @@
+# 0인 경우 북쪽,
+# 1인 경우 동쪽,
+# 2인 경우 남쪽,
+# 3인 경우 서쪽
+
+N, M = map(int, input().split())
+r, c, d = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
--- 6WEEK TEST/6WEEK Quiz/2805_bj.py
+++ 6WEEK TEST/6WEEK Quiz/2805_bj.py
@@ -1,1 +1,22 @@
-s
+N, M = map(int, input().split())
+tree_li = list(map(int, input().split()))
+
+low = 0
+high = max(tree_li)
+
+while low <= high:
+    mid = (low + high) // 2
+    hap = 0
+
+    for tree in tree_li:
+        if tree > mid:
+            hap += (tree - mid)
+
+    if hap >= M:
+        low = mid + 1
+    else:
+        high = mid - 1
+
+
+print(high)
+
