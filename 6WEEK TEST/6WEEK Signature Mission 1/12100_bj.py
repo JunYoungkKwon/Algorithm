3292678508a78643ddfb755b5d158fedb298b3e0
JUNYOUNG <gypig12@gachon.ac.kr>
2025-11-14 18:48:14 +0900

    11.12

--- 6WEEK TEST/6WEEK Signature Mission 1/12100_bj.py
+++ 6WEEK TEST/6WEEK Signature Mission 1/12100_bj.py
@@ -1,1 +1,32 @@
-aa
+N = int(input()) #보드크기
+board = [list(map(int, input().split())) for _ in range(N)]
+#보드는 무조건 5번 다 옮기는게 최대값
+#상하좌우 움직이면서 최대값 찾는것
+#일단 경우의 수는 4**5 상하좌우를 5번 랜덤
+
+# d = 0 1 2 3
+# 0  좌 상 우 하
+def move(d):
+    merged = False
+    if d == 0:
+        for i in range(N):
+            for k in range(len(board[i])-1):
+                if board[i][k] != 0:
+                    if board[i][k] == board[i][k+1]:
+                        board[i][k] = 2*board[i][k]
+
+                else:
+                    continue
+
+
+
+
+
+def find_max():
+    max_n = 0
+    for i in range(N):
+        max_n = max(max_n, max(board[i]))
+    return max_n
+
+
+print(find_max())
---
+++ 6WEEK TEST/6WEEK Signature Mission 1/13460_bj.py
@@ -0,0 +1,4 @@
+게임의 목표는 빨간 구슬을 구멍을 통해서 빼내는 것이다. 이때, 파란 구슬이 구멍에 들어가면 안 된다.
+
+N, M = map(int, input().split())
+board = [list(input().rstrip()) for _ in range(N)]
